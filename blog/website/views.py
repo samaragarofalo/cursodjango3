3e89898067e01b1f7cf53933312bd7849a9bf4a8
samaragarofalo <samaragarofalo@icloud.com>
2020-08-20 18:29:18 -0300

    Aulas sobre comandos do sistema de template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def hello_blog(request):
+    lista = [
+        'Django','Python','Git','Html','Banco de dados','Linux',
+        'Ngix','Uwsgi','Systemctl'
+    ]
+    data = {'name':'Curso de Django 3','lista_tecnologia': lista}
+    return render(request,'index.html',data)
+
+
